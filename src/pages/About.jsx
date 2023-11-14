c9d5c774e005402730423b134ce207b2ff5fe6b6
atheo810 <atheos810@gmail.com>
2023-11-14 15:49:23 +0800

    feat: about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="text-white flex flex-col justify-center mx-auto h-[600px] items-center">
+      <h1 className="md:text-6xl sm:text-5xl font-bold">About me</h1>
+      <p className="md:text-3xl sm:text-2xl font-bold">
+        Hi Patriot here, Im College student in INSTIKI also Core Team at GDSC
+        INSTIKI
+      </p>
+    </div>
+  );
+}
