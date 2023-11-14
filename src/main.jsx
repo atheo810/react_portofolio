c3068fa41ab5f01f3ffcf06ae030c75755a20b33
atheo810 <atheos810@gmail.com>
2023-11-14 15:49:14 +0800

    feat: router main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Root
 import App from "./App.jsx";
 import "./index.css";
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// Pages
+import Error from "./pages/Error.jsx";
+import About from "./pages/About.jsx";
+import PageHome from "./pages/PageHome.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <PageHome />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
