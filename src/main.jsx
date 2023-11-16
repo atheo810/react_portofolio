a2280b986971ea970e9f16b26532611304bff528
atheo810 <atheos810@gmail.com>
2023-11-16 21:44:46 +0800

    feat: structure main

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Root
-import App from "./App.jsx";
-import "./index.css";
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// Pages
-import Error from "./pages/Error.jsx";
-import About from "./pages/About.jsx";
-import PageHome from "./pages/PageHome.jsx";
-import PageResource from "./pages/PageResource.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <PageHome />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/resource",
-        element: <PageResource />,
-      },
-    ],
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
