95d5ef4170651947b904a4b6c907228328d71d45
atheo810 <atheos810@gmail.com>
2023-11-17 14:19:48 +0800

    feat: resource style

--- src/pages/PageResource.jsx
+++ src/pages/PageResource.jsx
@@ -1,4 +1,8 @@
+// Mui
+import { Box, Card, CardActions, CardContent, Typography } from "@mui/material";
+// react
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function PageResource() {
   const [dataApi, setDataApi] = useState([]);
@@ -15,15 +19,39 @@ export default function PageResource() {
     getApi().catch(console.error);
   }, []);
   return (
-    <div className="text-white">
-      tampil
-      <ul>
-        {dataApi.map((data) => (
-          <li key={data.id} className="p-4 hover:underline">
-            {data.name}
-          </li>
-        ))}
-      </ul>
+    <div className=" flex flex-wrap w-full">
+      {dataApi.map((data) => (
+        <Box
+          // sx={{ minWidth: 300, maxWidth: 300 }}
+          key={data.id}
+          className="p-2 max-sm:w-full sm:w-full md:max-w-[350px] md:min-w-[350px] md:min-h-[250px] md:max-h-[250px]"
+        >
+          <Card
+            sx={{
+              bgcolor: "#000000",
+              borderRadius: "15px",
+            }}
+            className="border border-white"
+          >
+            <CardContent>
+              <Typography color="#FFFFFF" sx={{ fontSize: 20 }} gutterBottom>
+                {data.name}
+              </Typography>
+              <Typography color="#FFFFFF" sx={{ mb: 1.5 }}>
+                {data.description}
+              </Typography>{" "}
+              <Typography color="#FFFFFF" sx={{ mb: 1.5 }}>
+                {data.updated_at}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Link className="text-white font-semibold" to={data.html_url}>
+                Go to Repo
+              </Link>
+            </CardActions>
+          </Card>
+        </Box>
+      ))}
     </div>
   );
 }
