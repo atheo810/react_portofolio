80e46d80b3e2de5112577c24f799579d8c9edb45
atheo810 <atheos810@gmail.com>
2024-04-22 21:09:11 +0800

    fix: renaming

--- src/components/Vue.jsx
+++ src/components/Vue.jsx
@@ -7,9 +7,9 @@ export default function Vue() {
         <div className="flex flex-col justify-center">
           <p className="text-[#859DFF] font-bold text-xl">React</p>
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 text-white">
-            Reat Library
+            Vue Library
           </h1>
-          <p className="text-white">React Library Use for making Front-End</p>
+          <p className="text-white">Vue Library Use for making Front-End</p>
           <button className="bg-[#1A27BA] text-white w-[200px] rounded-md font-bold my-6 md:mx-0 mx-auto py-3 ">
             Get Started
           </button>
